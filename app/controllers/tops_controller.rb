4524807149f77dacefa0853705fbb74d4eaabb56
milky <msy.9618@icloud.com>
2020-09-21 12:37:40 +0900

    貯金合計金額の表示設定

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,13 +1,19 @@
 class TopsController < ApplicationController
   def index
+    # ユーザー登録していないと表示されない
     if current_user.present?
+      # 目標の表示
       user = User.find(current_user.id)
       @goal = user.goal
+    # 目標の表示ここまで
+    
+    # 貯金合計金額の表示
+      user = User.find(current_user.id)
+      income_sum = user.incomes.sum(:price)
+      outgo_sum = user.outgos.sum(:price)
+      @total = income_sum - outgo_sum
+    # 貯金合計金額の表示ここまで
     end
-    user = User.find(current_user.id)
-    income_sum = user.incomes.sum(:price)
-    outgo_sum = user.outgos.sum(:price)
-    @total = income_sum - outgo_sum
   end
 
   def show

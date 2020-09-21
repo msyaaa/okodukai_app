a2fe667db8c99434b5cd852526bdaeb0efde7040
milky <msy.9618@icloud.com>
2020-09-21 12:29:05 +0900

    マイナス制限（支出操作）

--- app/controllers/outgos_controller.rb
+++ app/controllers/outgos_controller.rb
@@ -4,10 +4,17 @@ class OutgosController < ApplicationController
   end
 
   def create
+    user = User.find(current_user.id)
+    income_sum = user.incomes.sum(:price)
+    outgo_sum = user.outgos.sum(:price)
+    total = income_sum - outgo_sum
     @outgo = Outgo.new(outgo_params)
-    if @outgo.valid?
+    if @outgo.valid? && total - outgo_params[:price].to_i >= 0
       @outgo.save
       redirect_to root_path
+    elsif total - outgo_params[:price].to_i < 0
+      flash.now[:alert] = '残(のこ)っているお金が足りないよ！入力をやり直してね！'
+      render :new
     else
       render :new
     end
@@ -18,14 +25,34 @@ class OutgosController < ApplicationController
   end
 
   def update
+    user = User.find(current_user.id)
+    income_sum = user.incomes.sum(:price)
+    outgo_sum = user.outgos.sum(:price)
+    total = income_sum - outgo_sum
     @outgo = Outgo.find(params[:id])
     # user = User.find(current_user.id)
     # @incomes = user.incomes.order(date: :desc)
     # @outgos = Outgo.where(user_id: current_user.id).order(date: :desc)
-    if @outgo.update(outgo_params)
-      redirect_to top_path(current_user.id)
+    # if @outgo.update(outgo_params) && total - outgo_params[:price].to_i >= 0
+    if (total + @outgo.price >= outgo_params[:price].to_i) && outgo_params[:price] != ""
+      if @outgo.update(outgo_params)
+        redirect_to top_path(current_user.id)
+      else
+        render :edit
+      end
+    elsif total + @outgo.price < outgo_params[:price].to_i
+      if @outgo.update(outgo_params)
+        flash.now[:alert] = '残(のこ)っているお金が足りないよ！入力をやり直してね！'
+        render :edit
+      else
+        render :edit
+      end
     else
-      render :edit
+      if @outgo.update(outgo_params)
+        render :edit
+      else
+        render :edit
+      end
     end
   end
 
@@ -37,6 +64,7 @@ class OutgosController < ApplicationController
       redirect_to top_path(current_user.id)
     else
       render 'tops/show'
+      # redirect_to 
     end
   end
 

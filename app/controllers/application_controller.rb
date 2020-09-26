aba5e422d679a57c2d4fb50fc85422aa12359954
milky <msy.9618@icloud.com>
2020-09-26 18:31:47 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index]
   before_action :basic_auth
+
+  def after_sign_in_path_for(resource)
+    root_path(resource)
+  end
   private
 
   def basic_auth

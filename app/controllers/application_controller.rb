af59b7cf5d1efe6f2bd35f5ceb86f5aa99183e21
milky <msy.9618@icloud.com>
2020-09-27 21:49:21 +0900

    Basic認証解除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     root_path(resource)
   end
-  private
+  # private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  #   end
+  # end
 end
